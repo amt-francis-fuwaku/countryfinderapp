8dc9ca9e9f0a6b137bc4860ebb82c60b1947117b
francisfuwaku <francisfuwaku@amalitech.org>
2023-05-31 10:53:41 -0700

    initial app commmit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+function App() {
+    const name: string = "Francis";
+    console.log("🚀 ~ file: App.tsx:4 ~ App ~ name:", name);
+    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+export default App;
